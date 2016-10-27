9d30ecff296d89dca0afb7813c03aacf0cf3e38f
David Greiner <mail@david-greiner.me>
2016-10-27 11:34:28 +0200

    Remove useragent from parameters
    Fixes isIn generation

--- R/getPMML.R
+++ R/getPMML.R
@@ -83,7 +83,7 @@ getPMML <- function(json_data){
   ### data preprocessing to correct type (factor, numeric) ###
   
   sensorvalues <- subset(sensorvalues, select=-c(grep("timestamp", colnames(sensorvalues)),grep("id", colnames(sensorvalues)),grep("absolute", colnames(sensorvalues))))
-  sensorvalues$useragent <- as.factor(sensorvalues$useragent)
+  #sensorvalues$useragent <- as.factor(sensorvalues$useragent)
   sensorvalues$label <- as.factor(tolower(sensorvalues$label))
   sensorvalues[,-c(grep("useragent", colnames(sensorvalues)),grep("label", colnames(sensorvalues)))] <- sapply(sensorvalues[, -c(grep("useragent", colnames(sensorvalues)),grep("label", colnames(sensorvalues)))], as.numeric)
 
